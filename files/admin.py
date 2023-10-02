a6cff135f3138eaeb720af7b028d2b9180e2435d
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-02 16:37:26 +0300

    add admin.py

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import File
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file', 'uploaded_at', 'processed')
+    list_editable = ('file')
