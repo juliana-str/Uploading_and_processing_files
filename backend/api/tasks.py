1bd668c2de60130ed417123593b9a86151a8eed1
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-03 17:17:54 +0300

    add tasks

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,13 +1,12 @@
-from celery import task
-
+from celery import shared_task
 from .models import File
 
 
-@task
+@shared_task
 def processing_files(file_id):
     """Задача для обработки загруженного файла."""
     file = File.objects.get(id=file_id)
     if file:
-        file.processed = True
+        file.processed = 'True'
         return file
     raise ValueError('Файл не должен быть пустым!')
