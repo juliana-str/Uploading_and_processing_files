f4b1aa3d41f552ffc0c8c298694f5bc54cd2f7f5
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-03 14:10:01 +0300

    fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-from backend.api.views import FileViewSet, UploadViewSet
+from .views import FileViewSet, UploadViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('upload', UploadViewSet, basename='upload')
-router.register('api', FileViewSet, basename='api')
+router.register('files', FileViewSet, basename='files')
 
 
 urlpatterns = [
