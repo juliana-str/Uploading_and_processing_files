af306c04b44e78fb3ff3f3e582d55eb4608fb830
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-03 14:09:44 +0300

    fix views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,27 @@
-from django.db.migrations import serializer
 from requests import Response
 from rest_framework import mixins, status
 from rest_framework.viewsets import GenericViewSet
 
 from .models import File
-from .serializers import FileSerializer
+from .serializers import FileListSerializer, FilePostSerializer
+from .tasks import processing_files
 
 
 class FileViewSet(mixins.ListModelMixin, GenericViewSet):
     """Вьюсет для просмотра списка загруженных файлов."""
-    serializer = FileSerializer
+    serializer_class = FileListSerializer
     queryset = File.objects.all()
 
 
 class UploadViewSet(mixins.CreateModelMixin, GenericViewSet):
     """Вьюсет для загрузки файла."""
-    serializer_class = FileSerializer
+    serializer_class = FilePostSerializer
 
     def create(self, request, *args, **kwargs):
         """Метод загрузки файла."""
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(
+            data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        file = File.objects.create(request.data)
+        processing_files.delay(file.id)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
