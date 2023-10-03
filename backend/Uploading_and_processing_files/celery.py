7af6d8833c61992738f9c968cf3b480d03a01819
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-03 11:35:49 +0300

    initial commit

---
+++ backend/Uploading_and_processing_files/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Uploading_and_processing_files.settings')
+
+app = Celery('api')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
