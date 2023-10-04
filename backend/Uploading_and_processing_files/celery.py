9941894dacb36a822d6bef4b149cc16b7edff9dd
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-04 09:10:20 +0300

    fix celery

--- backend/Uploading_and_processing_files/celery.py
+++ backend/Uploading_and_processing_files/celery.py
@@ -9,8 +9,3 @@ app = Celery('api')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
