cfc0dc0d1dcc01c31fb900ef946a23e7278d8b78
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-05 18:54:07 +0300

    fix debug

--- backend/Uploading_and_processing_files/settings.py
+++ backend/Uploading_and_processing_files/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'uploadproject.hopto.org', '158.160.30.4']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
