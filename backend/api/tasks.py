6089c8e4c6664f6eab3d55a1d8d9b52e36506c7c
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-05 18:24:54 +0300

    fix task

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -5,24 +5,29 @@ import requests
 from celery import task
 from celery.utils.log import get_logger
 
+from .models import File
+
 logger = get_logger(__name__)
 
 
 @task
 def processing_files(file):
     """Задача для обработки загруженного файла."""
-    link = file.get('file')
-    try:
-        response = requests.get(link)
-        file_for_processing = response.text
-        search = re.findall(r'https:', file_for_processing)
-        if not search:
-            logging.debug('Файл проверен ссылок нет.')
-            file['processed'] = 'True'
-            logging.debug('Файл обработан.')
-        else:
-            logging.debug('Файл содержит неизвестные ссылки.')
+    # link = file.get('file')
+    # try:
+    #     response = requests.get(link)
+    #     file_for_processing = response.text
+    #     search = re.findall(r'https:', file_for_processing)
+    #     if not search:
+    #         logging.debug('Файл проверен ссылок нет.')
+    #     else:
+    #         logging.debug('Файл содержит неизвестные ссылки.')
 
+    try:
+        file_id = file.get('id')
+        File.objects.filter(id=file_id).update(processed='True')
+        logging.debug('Файл обработан.')
     except ProcessLookupError as error:
         logging.error(f'Файл не обработан! {error}.', exc_info=True)
         raise SystemError(f'Файл не обработан! {error}')
+
