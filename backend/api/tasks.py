3f6f8f4fc50513afbaa054a4d748f2c5a45bec2c
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-05 12:55:23 +0300

    fix tasks

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,5 +1,10 @@
+import logging
+
 from celery import task
 from celery.utils.log import get_logger
+from rest_framework.generics import get_object_or_404
+
+from .models import File
 
 logger = get_logger(__name__)
 
@@ -7,8 +12,13 @@ logger = get_logger(__name__)
 @task
 def processing_files(file):
     """Задача для обработки загруженного файла."""
-    with open(file, "r", encoding='utf-8') as f:
-        text = f.read()
-    if len(text) > 0:
-        file.processed = 'True'
-    raise ValueError('Файл не должен быть пустым!')
+    uploaded_file = file.get('file').split('/')[-2:]
+    try:
+        with open(uploaded_file, "r", encoding='utf-8') as f:
+            text = f.readlines()
+            logging.debug(text)
+            uploaded_file['processed'] = 'True'
+            logging.debug('Файл обработан.' f'{len(text)} строк.')
+    except ProcessLookupError as error:
+        logging.error(f'Файл не обработан! {error}.', exc_info=True)
+        raise SystemError(f'Файл не сохранен! {error}')
